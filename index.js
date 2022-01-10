726aafddd91e1722586cc60ac750b8724ace7b88
Chang Luo <chang.luo@tum.de>
2022-01-10 15:12:48 +0100

    change header tag to span tag

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const util = require('hexo-util');
 const request = require('request-promise');
-const cdn = 'https://cdn.jsdelivr.net/npm/hexo-tag-bangumi@0.1.1/hexo-tag-bangumi.css';
+const cdn = 'https://cdn.jsdelivr.net/npm/hexo-tag-bangumi/hexo-tag-bangumi.css';
 const css = (hexo.config.bangumi && hexo.config.bangumi.css) ? hexo.config.bangumi.css : cdn;
 
 hexo.extend.tag.register('bangumi', function (args) {
     var id = args[0];
     return result = getAnimeInfo(id).then(res => {
         var bangumiFontLink = util.htmlTag('link', {
-          href: 'https://fonts.googleapis.com/css2?family=Noto+Sans+JP&display=swap',
-          rel: 'stylesheet'
+            href: 'https://fonts.googleapis.com/css2?family=Noto+Sans+JP&display=swap',
+            rel: 'stylesheet'
         });
 
         var bangumiHeader = util.htmlTag('div', {
@@ -16,20 +16,23 @@ hexo.extend.tag.register('bangumi', function (args) {
         }, util.htmlTag('img', {
             src: res.image,
             class: 'bangumi-locandina'
-        }) + util.htmlTag('h1', {}, res.name_cn, false) + util.htmlTag('h4', {}, res.name, false) + util.htmlTag('div', {}, util.htmlTag('span', {
+        }) + util.htmlTag('span', {
+            id: "hexo-bangumi-header"
+        }, res.name_cn, false) + util.htmlTag('span', {
+            id: "hexo-bangumi-subheader"
+        }, res.name, false) + util.htmlTag('div', {}, util.htmlTag('span', {
             class: 'bangumi-rankspan'
-        }, "排名 " + util.htmlTag('span',{
+        }, "排名 " + util.htmlTag('span', {
             class: 'bangumi-rank'
-        }, "#"+res.rank, false) , false) + util.htmlTag('span', {
+        }, "#" + res.rank, false), false) + util.htmlTag('span', {
             class: 'bangumi-rankspan'
-        }, "话数 " + util.htmlTag('span',{
+        }, "话数 " + util.htmlTag('span', {
             class: 'bangumi-rank'
-        }, res.eps_count, false) , false) + util.htmlTag('span', {
+        }, res.eps_count, false), false) + util.htmlTag('span', {
             class: 'bangumi-score'
         }, res.score, false) + util.htmlTag('span', {
             style: 'color:#545454'
-        }, "&nbsp;"+res.score_cn , false) , false) , false);
-        // + util.htmlTag('span',{},res.score));
+        }, "&nbsp;" + res.score_cn, false), false), false);
 
 
         var bangumiDesc = util.htmlTag('div', {
@@ -63,7 +66,7 @@ async function getAnimeInfo(id) {
 
         if (res.id) {
             let score_cn;
-            switch(Math.floor(res.rating.score)) {
+            switch (Math.floor(res.rating.score)) {
                 case 1: score_cn = "不忍直视"; break;
                 case 2: score_cn = "很差"; break;
                 case 3: score_cn = "差"; break;
@@ -76,17 +79,16 @@ async function getAnimeInfo(id) {
                 default: score_cn = "暂无评分";
             }
             var data = {
-                name : res.name,
-                name_cn : res.name_cn,
-                summary : res.summary,
-                eps_count : res.eps_count,
-                score : res.rating.score,
-                rank : res.rank,
-                image : res.images.large,
+                name: res.name,
+                name_cn: res.name_cn,
+                summary: res.summary,
+                eps_count: res.eps_count,
+                score: res.rating.score,
+                rank: res.rank,
+                image: res.images.large,
                 score_cn: score_cn
             }
         }
-
         return data;
     });
 }
